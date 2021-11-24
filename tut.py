2a9eb1c5fd09e096ff78ad2dbe68da8e8c14ed99
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-11-24 11:58:00 +0530

    Add files via upload

---
+++ tut.py
@@ -0,0 +1,17 @@
+var1 = " Hello world "
+var2 = 4
+var3 = 32.4
+var4 = "Coders"
+var5 = "3"
+var6 = "5"
+print(10 * "hello world coders\n")
+
+print(int(var5)+int(var6))     # Type casting......
+print(type(var1))
+print(var1)
+print(type(var2))
+print(var2)
+print(type(var3))
+print(var3)
+print(var2+var3)
+print(var1+var4)
