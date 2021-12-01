526c203fb9fda0cc121a2ed53778f01c85b0af2f
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-01 18:24:34 +0530

    Add files via upload

---
+++ tryexception.py
@@ -0,0 +1,9 @@
+num1 = input("Enter a nos ")
+num2 = input("Enter a nos ")
+try:
+    print("Sum of these two nos : ", int(num1)+int(num2))
+
+except Exception as e:
+    print(e)
+
+print("This Line is very important")
