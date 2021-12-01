e42d9399e1f398862d3a7e16ac2b2bea2484b4ee
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-01 19:20:14 +0530

    Add files via upload

---
+++ FileIoBasis.py
@@ -0,0 +1,1 @@
+# File IO Basis# r - Open file for read  default mode# w - Open file for write# x - Creates file if not exists# a - add more content to file# t - text mode# b - binary mode# + - read mode /write mode
