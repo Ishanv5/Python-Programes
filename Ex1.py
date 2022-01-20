d4c65c0a6b32f1b2ab3a716a7cdf809ec109681c
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-20 23:14:41 +0530

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,13 @@
+from codecs import backslashreplace_errors
+from pickletools import floatnl
+
+
+# Print these foll
+# this is \\ double backslash
+# this is /\/\/\/\/\
+#     he is     awesome
+#     \"  \n \t \'
+print("this is \\\\ doble backslash")
+print("this is /\\/\\/\\/\\/\\  mountains")
+print("he is\tawesome")
+print('\\" \\n \\t \\\'')
