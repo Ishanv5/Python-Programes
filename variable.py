5a1429103304be16937ffb08a4cd1b0485b2e658
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-21 23:02:22 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -7,3 +7,7 @@ print(name)
 print(type(name))
 user_name="Ishan512"  #Snake case writing
 userName="Ishanv" #Camel Case writing
+name, age="ishan","18"
+print("hello " + name + " your age is " + age)
+x=y=z=10
+print(x+y+z)
