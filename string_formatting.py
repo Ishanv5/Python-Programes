6e5ffa05ee4b92f0afe57a7cc8bbd15a9cfe1483
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-21 23:23:03 +0530

    Add files via upload

---
+++ string_formatting.py
@@ -0,0 +1,7 @@
+name="Ishan"
+age=18
+print("hello " + name + " your age is " + str(age))
+# String formattig in python 3 
+print("hello {} your age is {}" .format(name,age))
+# String formattig in python 3.6
+print(f"hello {name} your age is {age}")
