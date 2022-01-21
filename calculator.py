c947676f31db3ab9335b861aabd0ed779d7c5f98
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-21 22:18:32 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print(2+3)
+print(2+3*4)
+print(2/4)  # floating pts  division 
+print(4//2) # Integer division
+print(5**2)  # Exponent 
+print(4**0.5)
+print(round(2**0.5,4))
+print(round(2**0.5,4))
+print(2**3/2*6-4*(3-4/2)) 
+print(3%2)
+print((2+3)*2)
+print((2+3)*5/2%6)
