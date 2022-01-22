1580de0273f518cac8448df8ffe280d9d99a8960
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-22 23:23:54 +0530

    Add files via upload

---
+++ string_method.py
@@ -0,0 +1,6 @@
+name="Ishan"
+print(len(name))  # To find length of string
+print(name.lower())  # to change case to lower caser(small)
+print(name.upper())  # To change case to upper case(capital)
+print(name.title())  
+print(name.count("h"))  
