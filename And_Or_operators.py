5d1e86fcbc1ae71a4867629c397f9fa34abc91af
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-26 22:25:43 +0530

    Add files via upload

---
+++ And_Or_operators.py
@@ -0,0 +1,13 @@
+name="abc"
+age=19
+if name=="abc" and age==19:
+    print("Condition true...")
+else:
+    print("Condition false...")
+name2="abc"
+age2=20
+if name2=="abc" or age2==19:
+    print("Condition true...")
+else:
+    print("Condition false...")
+    
