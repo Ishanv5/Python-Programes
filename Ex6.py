75fabbba6f9acbf770195cdfbd9733163f803367
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-26 22:36:37 +0530

    Add files via upload

---
+++ Ex6.py
@@ -0,0 +1,7 @@
+name=input("Enter name who are watching COCO.... : ")
+age=input("Enter age : ")
+age=int(age)
+if  (name[0]=='a' or name[0]=='A') and age >= 10:
+    print("You can watch COCO movie")
+else:
+    print("Sorry you cannot watch COCO")
