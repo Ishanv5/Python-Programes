92d02e171842cf34ebce562fc9d42218e1d0353a
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-01 22:20:03 +0530

    Add files via upload

---
+++ check_empty_ornot.py
@@ -0,0 +1,11 @@
+# name="abc"
+# if name: # true if string is not empty
+#     print("String is not empty")
+# else:
+#     print("String is empty")
+name=""
+if name: # true if string is not empty
+    print("String is not empty")
+else:
+    print("String is empty")
+    
