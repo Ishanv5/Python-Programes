ca2603601c3c2901179edd69a29827b99f6de2ca
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-08 22:31:28 +0530

    Add files via upload

---
+++ for_loop_string.py
@@ -0,0 +1,11 @@
+# name="Ishan"
+# for i in range(len(name)):
+#     print(name[i])
+# name="Ishan"
+# for i in name:
+#     print(i)
+n=input("enter a nos : " )
+total=0
+for i in n:
+    total+=int(i)
+print(total)
