7aaefc32b487b74865a5302cdf3697c291e868bd
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-12 11:00:58 +0530

    Add files via upload

---
+++ practice_session_for_function.py
@@ -0,0 +1,26 @@
+# def last_char(name):
+#     return name[-1]
+# print(last_char("Ishan"))
+# def odd_even(n):
+#     if n%2==0:
+#         return "even"
+ 
+#     return "odd"
+# def odd_even(n):
+#     if n%2==0:
+#         return "even"
+#     else:
+#         return "odd"
+    
+# print(odd_even(13))
+# def is_even(n):
+#     if n%2==0:
+#         return True
+#     else:
+#         return False
+    
+# print(is_even(1))
+def is_even(n):
+    return n%2==0
+
+print(is_even(3))
