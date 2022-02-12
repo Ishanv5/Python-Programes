af39819797c99b25824338f7b097c7335cd96071
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-12 10:44:23 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,17 @@
+# name="Ishan"
+# print(len(name))
+
+def add_two(a,b):
+    return a+b
+total=add_two(5,5)
+print(total)
+
+x=int(input("Enter a nos : "))
+y=int(input("Enter a nos : "))
+total2=add_two(x,y)
+print(total2)
+
+first_name=input("Type first name....:" )
+last_name=input("Type Last name ....: ")
+full_name=add_two(first_name,last_name)
+print(full_name)
