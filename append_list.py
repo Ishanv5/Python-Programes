7e90e38fe88882fe247a61963f9eeaaec56f59fc
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-15 22:47:33 +0530

    Add files via upload

---
+++ append_list.py
@@ -0,0 +1,8 @@
+# fruits=['Apple','Mango']
+# fruits.append("Chiku")
+# print(fruits)
+fruits=[]
+fruits.append("Mango")
+fruits.append("Apple")
+fruits.append("Chiku")
+print(fruits)
---
+++ more_method_to_add_in_list.py
@@ -0,0 +1,9 @@
+fruits=['Mango','Orange']
+# fruits.insert(1,"Chiku")
+# print(fruits)
+fruits2=['Apple','Grapes']
+# fruits3=fruits+fruits2
+# print(fruits3)
+fruits.extend(fruits2)
+print(fruits)
+print(fruits2)
