a3927bac86f1183d20ba5601a6a7c98ec27fa5a9
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-26 19:47:27 +0530

    Add files via upload

---
+++ Delete_data_from_list.py
@@ -0,0 +1,8 @@
+fruits=['Orange','Apple','pear',"Strawberry"]
+del fruits[0]
+print(fruits)
+fruits.remove('Apple')
+fruits.pop()
+print(fruits)
+fruits.pop(0)
+print(fruits)
---
+++ in_list.py
@@ -0,0 +1,5 @@
+fruits=['Orange','Apple','pear',"Strawberry","Mango"]
+if "Apple" in fruits:
+    print("Apple is Present")
+else:
+    print("Apple is Absent")
