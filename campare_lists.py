38aa940b90a91f9f6358b14f9a46f9ffe6bbd071
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-28 18:29:42 +0530

    Add files via upload

---
+++ campare_lists.py
@@ -0,0 +1,6 @@
+fruits1=['Orange','Apple','pear']
+fruits3=['Orange','Apple','pear']
+fruits2=["Strawberry","Mango","Kiwi"]
+print(fruits1==fruits2)
+print(fruits1==fruits3)
+print(fruits1 is fruits3)
---
+++ more_method.py
@@ -0,0 +1,12 @@
+fruits=['Orange','Apple','pear',"Strawberry","Mango"]
+print(fruits.count("Mango"))
+fruits.sort()
+print(fruits)
+number=[2,4,1,0]
+number.sort()
+print(number)
+print(sorted(number))
+# number.clear()
+# print(number)
+number_copy=number.copy()
+print(number_copy)
