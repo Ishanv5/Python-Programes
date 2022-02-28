43f245159485179c874c40f41868273090f84911
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-02-28 18:37:51 +0530

    Add files via upload

---
+++ Join_Spilt.py
@@ -0,0 +1,10 @@
+# ****************************  Spilt Method ***************************
+user_info="Ishan 18".split()
+user_info="Ishan,18".split(',')
+print(user_info)
+print(user_info)
+
+#  ****************************  Join Method ****************************
+user_info=["Ishan","18"]
+','.join(user_info)
+print(user_info)
