a9a9f876aaa82072495f47809d7253c73b589aaf
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-03-02 18:28:29 +0530

    Add files via upload

---
+++ List_String.py
@@ -0,0 +1,10 @@
+s="Ishan"
+s.title()
+print(s)
+  
+# ********************** List ******************
+l=["word1","word2","word3"]
+l.pop()
+print(l)
+l.append("word4")
+print(l)
---
+++ Looping_in_list.py
@@ -0,0 +1,8 @@
+fruits=['Orange','Apple','pear',"Strawberry","Mango"]
+# for fruit in fruits:
+#     print(fruit)
+    
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i+=1
