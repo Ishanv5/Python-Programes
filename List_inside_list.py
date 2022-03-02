a74229107db44892f254897acd736ad510338d06
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-03-02 18:40:57 +0530

    Add files via upload

---
+++ List_inside_list.py
@@ -0,0 +1,14 @@
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+print(matrix[0])
+print(matrix[2])
+for i in matrix:
+    for i2 in i:
+        print(i2)
+        
+print(matrix[1][1])
+print(matrix[1][2])
+
+
+s="String"
+print(type(s))
+print(type(matrix))
