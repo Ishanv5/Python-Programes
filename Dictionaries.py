541184a2090262402103e6f9ae4710b3c3cfb9f6
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-03-03 18:28:32 +0530

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,22 @@
+user={'name':'Ishan', 'age': 18}
+print(user)
+print(type(user))
+
+user1=dict(name='Ishan',age=18)
+print(user1)
+print(user1['name'])
+print(user1['age'])
+
+
+user_info={
+    'Name':'Ishan Vaghela',
+    'age':18,
+    'Fav_movies':['Spiderman','Dr Strange'],
+    'fav_tunes':['fariy tales','let me down slowly']
+}
+print(user_info)
+print(user_info['Fav_movies'])
+
+user_info2={}
+user_info2['Name']='Ishan'
+print(user_info2)
