1d0d76dd20dd468c236eb90915f9db876b09b682
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-03-09 17:59:33 +0530

    Add files via upload

---
+++ Advance_min_max_func.py
@@ -0,0 +1,9 @@
+nos=[1,2,3,4,5]
+print(max(nos))
+def func(item):
+    return len(item)
+    
+    
+names=['IshanV','Sujal','Meet']
+print(min(names,key=lambda item:len(item)))
+print(max(names,key=lambda item:len(item)))
